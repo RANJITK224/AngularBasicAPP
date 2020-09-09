d8d279ef22829f9bb816686795efb964af50e586
ranjitkumar.k <ranjitkumar.k@anblicks.com>
2020-09-09 16:55:07 +0530

    new project

---
+++ APIService/Models/DAL/EmployeeManager.cs
@@ -0,0 +1,45 @@
+﻿using APIService.Models.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIService.Models.DAL
+{
+    public class EmployeeManager : IDataRepository<Employee>
+    {
+        readonly DataBaseContext _dataBaseContext;
+        public EmployeeManager(DataBaseContext context)
+        {
+            _dataBaseContext = context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _dataBaseContext.Employees.ToList();
+        }
+        public Employee Get(long id)
+        {
+            return _dataBaseContext.Employees
+                  .FirstOrDefault(e => e.EmployeeId == id);
+        }
+        public void Add(Employee entity)
+        {
+            _dataBaseContext.Employees.Add(entity);
+            _dataBaseContext.SaveChanges();
+        }
+        public void Update(Employee employee, Employee entity)
+        {
+            employee.FirstName = entity.FirstName;
+            employee.LastName = entity.LastName;
+            employee.Email = entity.Email;
+            employee.DateOfBirth = entity.DateOfBirth;
+            employee.PhoneNumber = entity.PhoneNumber;
+            _dataBaseContext.SaveChanges();
+        }
+        public void Delete(Employee employee)
+        {
+            _dataBaseContext.Employees.Remove(employee);
+            _dataBaseContext.SaveChanges();
+        }
+    }
+}
---
+++ APIService/Models/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIService.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
